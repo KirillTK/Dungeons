f3a243581cc4e3fc343c35a63e7be53b6ef52ea9
KirillTK <kirill99.tkachev@yandex.by>
2019-04-13 17:45:13 +0300

    feature: compress and add new characters

--- game/src/shared/model/Path.ts
+++ game/src/shared/model/Path.ts
@@ -1,5 +1,5 @@
 export class Path {
-  public static  HERO_PATH = ['assets/json/hero.json'];
+  public static  HERO_PATH = ['assets/json/hero/archer.json', 'assets/json/hero/knight.json', 'assets/json/hero/wizzard.json'];
   public static  ENEMY_PATH = ['assets/json/enemy1.json','assets/json/enemy2.json','assets/json/enemy3.json'];
   public static  MAIN_THEME = 'assets/sounds/Dream.mp3';
 }
--- game/src/shared/services/character/character.service.ts
+++ game/src/shared/services/character/character.service.ts
@@ -2,13 +2,15 @@ import {Injectable} from '@angular/core';
 import {Character} from '../../model/character';
 import * as _ from 'lodash';
 import {NamesForEnemy} from '../../model/NamesForEnemy';
+import {Path} from "../../model/Path";
+import {CharacterSharedService} from "../character-shared.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
 
-  constructor() {
+  constructor(private sharedService: CharacterSharedService) {
   }
 
 
@@ -17,9 +19,9 @@ export class CharacterService {
   }
 
   getRandomName(): string {
-    const adject = NamesForEnemy.ADJECTIVES[_.random(NamesForEnemy.ADJECTIVES.length-1)];
-    const type = NamesForEnemy.TYPE[_.random(NamesForEnemy.TYPE.length-1)];
-    const name = NamesForEnemy.NAME[_.random(NamesForEnemy.NAME.length-1)];
+    const adject = NamesForEnemy.ADJECTIVES[_.random(NamesForEnemy.ADJECTIVES.length - 1)];
+    const type = NamesForEnemy.TYPE[_.random(NamesForEnemy.TYPE.length - 1)];
+    const name = NamesForEnemy.NAME[_.random(NamesForEnemy.NAME.length - 1)];
     return adject + ' ' + type + ' ' + name;
   }
 
@@ -27,13 +29,21 @@ export class CharacterService {
     const path = this.getRandomCharacter(characters);
     return fetch(path)
       .then(response => response.json())
-      .then((data:Character) => {
+      .then((data: Character) => {
         if (typeCharacter === 'enemy') {
           data.name = this.getRandomName();
           return data;
         }
         return data;
       });
+  }
+
+  async getUserCharacter() {
+    const userInfo = JSON.parse(window.localStorage.getItem('userInfo'));
+    this.sharedService.setHeroName(userInfo.nickName);
+    const result = await fetch(Path.HERO_PATH[userInfo.selectedHero.index]).then(response => response.json());
+    console.log(userInfo,result );
+    return result;
   }
 
 }
