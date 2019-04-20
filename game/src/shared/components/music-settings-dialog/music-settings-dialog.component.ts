e3a4e85d5a13f411fc2d4e3a12b0c85156dfabb6
KirillTK <kirill99.tkachev@yandex.by>
2019-04-20 23:49:14 +0300

    feature: create music settings dialog

---
+++ game/src/shared/components/music-settings-dialog/music-settings-dialog.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-music-settings',
+  templateUrl: './music-settings-dialog.component.html',
+  styleUrls: ['./music-settings-dialog.component.css']
+})
+export class MusicSettingsComponent  {
+    
+}
