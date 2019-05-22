ae24f707a023985d94704d08d34beb7a925d346f
KirillTK <kirill99.tkachev@yandex.by>
2019-05-22 23:04:10 +0300

    refactor: replace json data to const object

---
+++ game/src/app/common/enemies/enemies.ts
@@ -0,0 +1,35 @@
+const enemies = [
+    {
+        "health" : 100,
+        "pathCharacter": "./assets/enemy/enemy1/golem.gif",
+        "pathAttack": "./assets/enemy/enemy1/golem-attack.gif",
+        "pathDeath": "./assets/enemy/enemy1/golem-death.gif",
+        "pathHeat": "./assets/enemy/enemy1/golem-hurt.gif",
+        "pathWalkBack": "./assets/enemy/enemy1/golem-back.gif",
+        "pathWalkStraight": "./assets/enemy/enemy1/golem-straight.gif",
+        "type": "melee"
+      },
+      {
+        "health" : 100,
+        "pathCharacter": "./assets/enemy/enemy2/golem.gif",
+        "pathAttack": "./assets/enemy/enemy2/golem-attack.gif",
+        "pathDeath": "./assets/enemy/enemy2/golem-death.gif",
+        "pathHeat": "./assets/enemy/enemy2/golem-hurt.gif",
+        "pathWalkBack": "./assets/enemy/enemy2/golem-back.gif",
+        "pathWalkStraight": "./assets/enemy/enemy2/golem-straight.gif",
+        "type": "melee"
+      },
+      {
+        "health" : 100,
+        "pathCharacter": "./assets/enemy/enemy3/golem.gif",
+        "pathAttack": "./assets/enemy/enemy3/golem-attack.gif",
+        "pathDeath": "./assets/enemy/enemy3/golem-death.gif",
+        "pathHeat": "./assets/enemy/enemy3/golem-hurt.gif",
+        "pathWalkBack": "./assets/enemy/enemy3/golem-back.gif",
+        "pathWalkStraight": "./assets/enemy/enemy3/golem-straight.gif",
+        "type": "melee"
+      }
+
+]
+  
+export default enemies;
