047147b4fd8482f797bcd2e19e3f6577840819b0
KirillTK <kirill99.tkachev@yandex.by>
2019-05-25 13:20:07 +0300

    refactor: remove ngAfterViewChecked method

--- game/src/app/play-page/bar/bar.component.ts
+++ game/src/app/play-page/bar/bar.component.ts
@@ -1,19 +1,21 @@
+import { FightService } from './../../../shared/services/fight.service';
 import {
-  AfterViewChecked,
   ChangeDetectorRef,
   Component, ElementRef,
   Input,
   OnInit,
-  ViewChild
+  ViewChild,
+  AfterViewInit
 } from '@angular/core';
 import {CharacterSharedService} from '../../../shared/services/character-shared.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-bar',
   templateUrl: './bar.component.html',
   styleUrls: ['./bar.component.css']
 })
-export class BarComponent implements OnInit, AfterViewChecked {
+export class BarComponent implements OnInit, AfterViewInit {
 
   @ViewChild('heroHealth') heroHealth: ElementRef;
   @ViewChild('enemyHealth') enemyHealth: ElementRef;
@@ -29,29 +31,48 @@ export class BarComponent implements OnInit, AfterViewChecked {
 
   @ViewChild('enemyHP') enemyHP: ElementRef;
 
+  finishHeroAnimation$: Observable<any>;
+  finishEnemyAnimation$: Observable<any>;
 
-  constructor(private cd: ChangeDetectorRef, private characterSharedService: CharacterSharedService) {
+
+  constructor(private cd: ChangeDetectorRef, private characterSharedService: CharacterSharedService, private fight: FightService) {
   }
 
   ngOnInit() {
+    this.finishHeroAnimation$ = this.fight.finishHeroAnimation;
+    this.finishEnemyAnimation$ = this.fight.finishEnemyAnimation;
+
+    this.finishHeroAnimation$.subscribe(()=>{
+        console.log('finish hero attack');
+        setTimeout(()=>this.renderHP(),0)
+    });
+
+    this.finishEnemyAnimation$.subscribe(()=>{
+        console.log('finish enemy attack');
+        setTimeout(()=>this.renderHP(),0)
+    });
+
   }
 
-  ngAfterViewChecked() {
-    if (this.characterSharedService.getHeroHealth() !== undefined && this.characterSharedService.getEnemyHealth() !== undefined) {
-      this._enemyHealth = this.characterSharedService.getEnemyHealth();
-      this.enemyHealth.nativeElement.innerText = this._enemyHealth + ' / 100';
+  ngAfterViewInit() {
+    this.renderHP();
+  }
 
-      this._heroHealth = this.characterSharedService.getHeroHealth();
-      this.heroHealth.nativeElement.innerText = this._heroHealth + ' / 100';
+  private renderHP() {
+    this._enemyHealth = this.characterSharedService.getEnemyHealth();
+    this.enemyHealth.nativeElement.innerText = this._enemyHealth + ' / 100';
 
+    this._heroHealth = this.characterSharedService.getHeroHealth();
+    this.heroHealth.nativeElement.innerText = this._heroHealth + ' / 100';
 
-      this.enemyName.nativeElement.innerText = this.characterSharedService.getEnemyName();
-      this.heroName.nativeElement.innerText = this.characterSharedService.getHeroName();
+    console.log(this._enemyHealth, this._heroHealth)
 
-      this.calculateHero(this._heroHealth, this.heroHP);
-      this.calculateHero(this._enemyHealth, this.enemyHP);
+    this.enemyName.nativeElement.innerText = this.characterSharedService.getEnemyName();
+    this.heroName.nativeElement.innerText = this.characterSharedService.getHeroName();
 
-    }
+    this.calculateHero(this._heroHealth, this.heroHP);
+    this.calculateHero(this._enemyHealth, this.enemyHP);
+
   }
 
   calculateHero(health: number, hpElement: ElementRef): void {
