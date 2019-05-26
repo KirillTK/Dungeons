6bc767fc2b407eee264db48434c0a9eeb43d7b28
KirillTK <kirill99.tkachev@yandex.by>
2019-05-26 23:05:14 +0300

    feature: add difficult tasks

--- game/src/shared/model/DragOrder.ts
+++ game/src/shared/model/DragOrder.ts
@@ -108,3 +108,103 @@ export const dragOrder: Task[] = [
   }
 
 ];
+
+
+export const dragOrderDifficult2: Task[] = [
+  {
+    expression: 'tac',
+    result: 'cat',
+    damage: DAMAGE.ORDER_TASK
+  },
+  {
+    result: 'abolish',
+    expression: 'lishabo',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'addiction',
+    expression: 'cioaondtd',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'anger',
+    expression: 'regan',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'learn',
+    expression: 'earnl',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'change',
+    expression: 'geanch',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'understand',
+    expression: 'underandst',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'follow',
+    expression: 'owfoll',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'create',
+    expression: 'eatecr',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'spend',
+    expression: 'endsp',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'produce',
+    expression: 'ducepro',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'profit',
+    expression: 'ofitpr',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'addiction',
+    expression: 'ionaddict',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'referee',
+    expression: 'fereere',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'whistle',
+    expression: 'stlewhi',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'valuable',
+    expression: 'uableva',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'mirror',
+    expression: 'rrmior',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'justice',
+    expression: 'ticejus',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  },
+  {
+    result: 'district',
+    expression: 'rictdist',
+    damage: DAMAGE.ENG_TO_RUS_TASK
+  }
+  
+]
